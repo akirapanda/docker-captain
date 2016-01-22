a69366cfda7a7475be12599a397f17383f197b29
AkiraPanda <akirapanda@newnil.com>
2016-01-22 22:42:05 +0800

    test(codeclimate):fix coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,12 @@ require 'rack/test'
 
 require "codeclimate-test-reporter"
 
+
+ENV['CODECLIMATE_REPO_TOKEN'] = '0fca59f3fbdda7a93a6fb369a199841a4399171505dab657fdc90911ad9539b6'
+CodeClimate::TestReporter.start
+
+
+
 # set test environment
 Sinatra::Base.set :environment, :test
 Sinatra::Base.set :run, false
@@ -16,7 +22,8 @@ require './application'
 
 require File.join(File.dirname(__FILE__), '../application')
 
-CodeClimate::TestReporter.start
+
+
 
 RSpec.configure do |c|
   c.mock_with :rspec
